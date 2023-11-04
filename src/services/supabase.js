b6a581ae9691fd2d45fc6859f8641ccd6bc58889
AHMAD IQBAL BHATTI <ahmediqbalbhatti1@gmail.com>
2023-11-04 11:34:15 +0500

    Initial Commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,9 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://amkrkgrexshfpmnanhhv.supabase.co'
+const supabaseUrl = "https://amkrkgrexshfpmnanhhv.supabase.co"
 // const supabaseKey = process.env.SUPABASE_KEY
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e yJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFta3JrZ3JleHNoZnBtbmFuaGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2ODQyODYsImV4cCI6MjAxNDI2MDI4Nn0.s191Sd5P34SUUtVATBMDJl7jK7FmhuaelT3kmPtXNWs"
+console.log(process.env.SUPABASE_ACCESS_KEY);
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFta3JrZ3JleHNoZnBtbmFuaGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2ODQyODYsImV4cCI6MjAxNDI2MDI4Nn0.s191Sd5P34SUUtVATBMDJl7jK7FmhuaelT3kmPtXNWs"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
